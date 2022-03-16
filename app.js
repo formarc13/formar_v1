1f975eee1ddcf0fda69a2a8c71454aa1a1ebf3bc
Jonatan Céspedes <develop.jec@gmail.com>
2022-03-15 21:01:20 -0300

    Add header and footer styles

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const process = require('process');
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')))
--- public/js/header.js
+++ public/js/header.js
@@ -1,1 +1,12 @@
 /* JS HEADER */
+let menuButton = document.querySelector('.header_menuButton');
+let menu = document.querySelector('.header_menu');
+let menuCloseButton = document.querySelector('.header_menuClose');
+
+menuButton.addEventListener('click', () => {
+    menu.classList.add('active');
+})
+
+menuCloseButton.addEventListener('click', () => {
+    menu.classList.remove('active');
+})
